93b5b8e3e513a1553c9baef63fa8d63368a1f656
muhammadanang <muhanangmahrub@gmail.com>
2022-12-01 09:17:32 +0700

    added a link from homepage to about page

--- travel.js
+++ travel.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -13,25 +14,18 @@ app.set('view engine', 'handlebars')
 const port = process.env.PORT || 3000
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    `Express mulai di https://localhost:${port}; press Ctrl-C to terminate.`
-))
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Express berjalan pada http://localhost:${port}` + `; tekan Ctrl-C untuk terminate.`)
+    })
+} else {
+    module.exports = app
+}
