e94b83771a5820c542e20b4b3d20ec2755e8629e
muhammadanang <muhanangmahrub@gmail.com>
2022-12-04 20:13:00 +0700

    added a register function

--- travel.js
+++ travel.js
@@ -2,11 +2,22 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const fortune = require('./lib/fortune')
 const handlers = require('./lib/handlers')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {}
+            this._sections[name] = options.fn(this)
+            return null
+        },
+    },
 }))
 
 app.set('view engine', 'handlebars')
@@ -17,6 +28,13 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
+
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankyou)
+
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
 
 app.use(handlers.notFound)
 
